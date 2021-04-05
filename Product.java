fe1bd4e5e401c0e931be0e1f444902e6a427b7bc
Kis <e.kis@gmx.at>
2021-04-05 23:25:24 +0200

    Create Product class

---
+++ Product.java
@@ -0,0 +1,24 @@
+public class Product
+{
+	private String name = "Coca Cola";
+	
+	
+
+	public Product(String name)
+	{
+		
+		this.name = name;
+		
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+}
